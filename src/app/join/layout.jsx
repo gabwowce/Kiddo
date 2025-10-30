18db954e9bf7b5209673b161abaa0b0ea405c918
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2025-10-30 15:34:13 +0200

    Delete src/app/join directory

--- src/app/join/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="min-h-dvh grid place-items-center bg-neutral-100">
-      <div className="w-[394px] max-w-[94vw] h-[760px] rounded-3xl bg-black text-white overflow-hidden shadow-2xl">
-        {children}
-      </div>
-    </div>
-  );
-}
